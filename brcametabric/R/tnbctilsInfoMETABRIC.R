d5b9bdaebceb5ffee73110143669555c0ded21a3
kelgalla <kelgalla@gmail.com>
2020-09-20 22:57:56 -0400

    adding quartile analysis

--- brcametabric/R/tnbctilsInfoMETABRIC.R
+++ brcametabric/R/tnbctilsInfoMETABRIC.R
@@ -15,6 +15,9 @@ surFile <- "F:\\TNBC TILS\\tnbctils\\brcametabric\\processedData\\tnbcSurv.txt"
 sur <- read.delim(surFile, header=T, stringsAsFactors=F, na.strings=(""))
 sur$OS_STATUS2 <- ifelse(sur$OS_STATUS == "0:LIVING", 0, 1)
 
+dirSave <- "F:\\TNBC TILS\\tnbctils\\brcametabric\\cibersort\\intensity\\"
+dirSave2 <- "F:\\TNBC TILS\\tnbctils\\brcametabric\\cibersort\\intensity\\noSig\\"
+
 #----------------------------------
 # 199 samples
 #----------------------------------
@@ -66,6 +69,75 @@ cox <- coxph(Surv(OS_MONTHS, OS_STATUS2)~sum, data=sur)
 summary(cox)
 
 cox <- coxph(Surv(OS_MONTHS, OS_STATUS2)~sum+TUMOR_STAGE+AGE_AT_DIAGNOSIS+INFERRED_MENOPAUSAL_STATE, data=sur)
+summary(cox)
+
+#----------------------------------
+# CD8 quartiles
+#----------------------------------
+rel <- read.delim("F:\\TNBC TILS\\tnbctils\\brcametabric\\cibersort\\data\\CIBERSORT.Output_Abs_intensity.txt", header=T, stringsAsFactors=F)
+rel$Input.Sample <- gsub("\\.", "-", rel$Input.Sample)
+
+# 5 = CD8, 8 = CD4
+cell_idx <- 5
+cibersort <- rel[,c(1,cell_idx)]
+colnames(cibersort)[2] <- "sum"
+
+sur <- merge(sur, cibersort, by.x="PATIENT_ID", by.y="Input.Sample", all.x=T)
+sur <- sur[!(is.na(sur$sum)),]
+
+sur[sur$TUMOR_STAGE %in% c("1", "2"),]$TUMOR_STAGE <- "Stage I-II"
+sur[sur$TUMOR_STAGE %in% c("3"),]$TUMOR_STAGE <- "Stage III-IV"
+sur$TUMOR_STAGE <- factor(sur$TUMOR_STAGE, levels=c("Stage I-II", "Stage III-IV"), ordered=T)
+count(sur, 'TUMOR_STAGE')
+
+sur$INFERRED_MENOPAUSAL_STATE <- factor(sur$INFERRED_MENOPAUSAL_STATE, levels=c("Pre", "Post"), ordered=T)
+count(sur, 'INFERRED_MENOPAUSAL_STATE')
+
+quantile(sur$sum)
+
+sur$quartile <- cut(sur$sum, breaks=quantile(sur$sum), labels=c("Q1","Q2","Q3","Q4"), include.lowest=T)
+
+#tmp <- sur[order(sur$sum),c(1,34,35)]
+
+q1Num <- dim(sur[sur$quartile=="Q1",])[1]
+q2Num <- dim(sur[sur$quartile=="Q2",])[1]
+q3Num <- dim(sur[sur$quartile=="Q3",])[1]
+q4Num <- dim(sur[sur$quartile=="Q4",])[1]
+
+# km OS
+smpl.surv <- Surv(sur$OS_MONTHS, sur$OS_STATUS2)~sur$quartile
+sigTest <- survdiff(smpl.surv)
+pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+smpl.fit <- survfit(smpl.surv)
+summary(smpl.fit)
+pvalue
+
+#---
+# fancy plot
+# os
+width <- 2500
+#ratio <- 461/572
+ratio <- 547/600
+height <- trunc(width*ratio)
+png(paste0(dirSave2, "os.abs.T.cells.CD8.quartiles.2.png"), 
+    width=width, height=height, res=300)
+
+palette <- c("#B79F00", "#00BA38", "#619CFF", "#A58AFF")
+par(mar=c(5.1,5.1,4.1,2.1))
+plot(smpl.fit, lty=c(1,1),
+     xlab="Months", ylab="Overall Survival", col=palette, lwd=2, mark=108, cex=1.5, cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+legend(200, 1.03, legend=c(paste0("Q4, n=", q1Num), paste0("Q3, n=", q2Num), paste0("Q2, n=", q3Num), paste0("Q1, n=", q4Num)), 
+       lty=c(1,1), col=rev(palette), lwd=2, cex=1.5)
+
+dev.off()
+# fancy plot
+#---
+
+#n <- 7
+#hcl(h=seq(15, 375-360/n, length=n)%%360, c=100, l=65)
+
+# cox os
+cox <- coxph(Surv(OS_MONTHS, OS_STATUS2)~quartile, data=sur)
 summary(cox)
 
 #----------------------------------
@@ -172,6 +244,77 @@ cox <- coxph(Surv(OS_MONTHS, OS_STATUS2)~sum+TUMOR_STAGE+AGE_AT_DIAGNOSIS+INFERR
 summary(cox)
 
 cox <- coxph(Surv(OS_MONTHS, OS_STATUS2)~sum+AGE_AT_DIAGNOSIS, data=sur)
+summary(cox)
+
+
+#----------------------------------
+# T Cells gamma delta quartiles
+#----------------------------------
+rel <- read.delim("F:\\TNBC TILS\\tnbctils\\brcametabric\\cibersort\\data\\CIBERSORT.Output_Abs_intensity.txt", header=T, stringsAsFactors=F)
+rel$Input.Sample <- gsub("\\.", "-", rel$Input.Sample)
+
+# 5 = CD8, 8 = CD4
+cell_idx <- 11
+cibersort <- rel[,c(1,cell_idx)]
+colnames(cibersort)[2] <- "sum"
+
+sur <- merge(sur, cibersort, by.x="PATIENT_ID", by.y="Input.Sample", all.x=T)
+sur <- sur[!(is.na(sur$sum)),]
+
+sur[sur$TUMOR_STAGE %in% c("1", "2"),]$TUMOR_STAGE <- "Stage I-II"
+sur[sur$TUMOR_STAGE %in% c("3"),]$TUMOR_STAGE <- "Stage III-IV"
+sur$TUMOR_STAGE <- factor(sur$TUMOR_STAGE, levels=c("Stage I-II", "Stage III-IV"), ordered=T)
+count(sur, 'TUMOR_STAGE')
+
+sur$INFERRED_MENOPAUSAL_STATE <- factor(sur$INFERRED_MENOPAUSAL_STATE, levels=c("Pre", "Post"), ordered=T)
+count(sur, 'INFERRED_MENOPAUSAL_STATE')
+
+quantile(sur$sum)
+
+sur$quartile <- cut(sur$sum, breaks=quantile(sur$sum), labels=c("Q1","Q2","Q3","Q4"), include.lowest=T)
+
+#tmp <- sur[order(sur$sum),c(1,34,35)]
+
+q1Num <- dim(sur[sur$quartile=="Q1",])[1]
+q2Num <- dim(sur[sur$quartile=="Q2",])[1]
+q3Num <- dim(sur[sur$quartile=="Q3",])[1]
+q4Num <- dim(sur[sur$quartile=="Q4",])[1]
+
+# km OS
+smpl.surv <- Surv(sur$OS_MONTHS, sur$OS_STATUS2)~sur$quartile
+sigTest <- survdiff(smpl.surv)
+pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+smpl.fit <- survfit(smpl.surv)
+summary(smpl.fit)
+pvalue
+
+#---
+# fancy plot
+# os
+width <- 2500
+#ratio <- 461/572
+ratio <- 547/600
+height <- trunc(width*ratio)
+png(paste0(dirSave, "os.abs.T.cells.gamma.delta.quartiles.2.png"), 
+    width=width, height=height, res=300)
+
+palette <- c("#B79F00", "#00BA38", "#619CFF", "#A58AFF")
+par(mar=c(5.1,5.1,4.1,2.1))
+par(xpd=TRUE)
+plot(smpl.fit, lty=c(1,1),
+     xlab="Months", ylab="Overall Survival", col=palette, lwd=2, mark=108, cex=1.5, cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+legend(-10, 0.38, legend=c(paste0("Q4, n=", q1Num), paste0("Q3, n=", q2Num), paste0("Q2, n=", q3Num), paste0("Q1, n=", q4Num)), 
+       lty=c(1,1), col=rev(palette), lwd=2, cex=1.5)
+
+dev.off()
+# fancy plot
+#---
+
+#n <- 7
+#hcl(h=seq(15, 375-360/n, length=n)%%360, c=100, l=65)
+
+# cox os
+cox <- coxph(Surv(OS_MONTHS, OS_STATUS2)~quartile, data=sur)
 summary(cox)
 
 #----------------------------------
@@ -400,4 +543,49 @@ for (cell_idx in 2:(dim(rel)[2]-3)){
 
 cellTblOS <- pvalTbl[pvalTbl$survival=="os",]
 cellTblOS$fdr <- p.adjust(cellTblOS$pval, method="fdr")
+
+#----------------------------------
+# all immune types quartiles for purposes of calculating fdr
+#----------------------------------
+rel <- read.delim("F:\\TNBC TILS\\tnbctils\\brcametabric\\cibersort\\data\\CIBERSORT.Output_Abs_intensity.txt", header=T, stringsAsFactors=F)
+rel$Input.Sample <- gsub("\\.", "-", rel$Input.Sample)
+
+pvalTbl <- data.frame(cellType=character(), survival=character(), splitType=character(), pval=numeric())
+
+#cell_idx=2
+for (cell_idx in 2:(dim(rel)[2]-3)){
+  
+  cibersort <- rel[,c(1,cell_idx)]
+  cellType <- colnames(cibersort)[2]
+  colnames(cibersort)[2] <- "sum"
+  
+  surv <- merge(sur, cibersort, by.x="PATIENT_ID", by.y="Input.Sample", all.x=T)
+  
+  # analysis of cibersort
+  surv <- surv[!(is.na(surv$sum)),]
+  
+  p <- tryCatch({ # some immune cells have too many 0's such that they can't be split into quartiles
+    
+    surv$quartile <- cut(surv$sum, breaks=quantile(surv$sum), labels=c("Q1","Q2","Q3","Q4"), include.lowest=T)
+    
+    # km OS
+    smpl.surv <- Surv(surv$OS_MONTHS, surv$OS_STATUS2)~surv$quartile
+    sigTest <- survdiff(smpl.surv)
+    pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+    
+  }, error = function(e) {
+    
+    return(NA)
+    
+  })
+  
+  comp1 <- c(cellType, "os", "quartile", p)
+  pvalTbl[nrow(pvalTbl)+1,] <- comp1
+  
+}
+
+cellTblOS <- pvalTbl[pvalTbl$survival=="os",]
+cellTblOS$fdr <- p.adjust(cellTblOS$pval, method="fdr")
+
+
 
--- brcatcga/analysis/R/tnbctilsInfo.R
+++ brcatcga/analysis/R/tnbctilsInfo.R
@@ -9,10 +9,13 @@
 
 library(survival)
 library(plyr)
+library(dplyr)
 library(survminer)
 
 surFile <- "F:\\TNBC TILS\\tnbctils\\brcatcga\\analysis\\R\\tnbc.tils.append2.surv.idc.txt"
 sur <- read.delim(surFile, header=T, stringsAsFactors=F, na.strings=(""))
+
+dirSave <- "F:\\TNBC TILS\\tnbctils\\brcatcga\\analysis\\R\\cibersort\\fpkm\\"
 
 #----------------------------------
 # 133 samples
@@ -177,6 +180,106 @@ summary(cox)
 
 cox <- coxph(Surv(dfs_days, dfs_status)~sum+pathologic_stage, data=sur)
 summary(cox)
+
+#----------------------------------
+# CD8 quartiles
+#----------------------------------
+rel <- read.delim("F:\\TNBC TILS\\tnbctils\\brcatcga\\analysis\\R\\cibersort\\data\\CIBERSORT.Output_Abs_fpkm.txt", header=T, stringsAsFactors=F)
+rel$Input.Sample <- gsub("\\.", "-", rel$Input.Sample)
+
+# 5 = CD8, 8 = CD4
+cell_idx <- 5
+cibersort <- rel[,c(1,cell_idx)]
+colnames(cibersort)[2] <- "sum"
+
+sur <- merge(sur, cibersort, by.x="bcr_patient_barcode", by.y="Input.Sample", all.x=T)
+sur <- sur[!(is.na(sur$sum)),]
+
+sur[sur$race=="ASIAN" & !is.na(sur$race),]$race <- NA
+sur$race <- factor(sur$race, levels=c(unique(sur$race)))
+count(sur, 'race')
+
+sur[sur$pathologic_stage %in% c("Stage I", "Stage IA"),]$pathologic_stage <- "Stage I-II"
+sur[sur$pathologic_stage %in% c("Stage II", "Stage IIA", "Stage IIB"),]$pathologic_stage <- "Stage I-II"
+sur[sur$pathologic_stage %in% c("Stage III", "Stage IIIA", "Stage IIIB", "Stage IIIC"),]$pathologic_stage <- "Stage III-IV"
+sur[sur$pathologic_stage %in% c("Stage IV"),]$pathologic_stage <- "Stage III-IV"
+sur$pathologic_stage <- factor(sur$pathologic_stage, levels=c("Stage I-II", "Stage III-IV"), ordered=T)
+count(sur, 'pathologic_stage')
+
+sur[sur$menopause_status %in% c("Indeterminate (neither Pre or Postmenopausal)"),]$menopause_status <- NA
+sur[sur$menopause_status %in% c("Peri (6-12 months since last menstrual period)"),]$menopause_status <- NA
+sur$menopause_status <- factor(sur$menopause_status, levels=c("Pre (<6 months since LMP AND no prior bilateral ovariectomy AND not on estrogen replacement)",
+                                                              "Post (prior bilateral ovariectomy OR >12 mo since LMP with no prior hysterectomy)"), ordered=T)
+count(sur, 'menopause_status')
+
+quantile(sur$sum)
+
+sur$quartile <- cut(sur$sum, breaks=quantile(sur$sum), labels=c("Q1","Q2","Q3","Q4"), include.lowest=T)
+
+#tmp <- sur[order(sur$sum),c(1,75,76)]
+
+q1Num <- dim(sur[sur$quartile=="Q1",])[1]
+q2Num <- dim(sur[sur$quartile=="Q2",])[1]
+q3Num <- dim(sur[sur$quartile=="Q3",])[1]
+q4Num <- dim(sur[sur$quartile=="Q4",])[1]
+
+#sur$quartileNum <- ntile(sur$sum, 4)
+#sur$quartile <- NA
+#sur[sur$quartileNum==1,]$quartile <- "Q1"
+#sur[sur$quartileNum==2,]$quartile <- "Q2"
+#sur[sur$quartileNum==3,]$quartile <- "Q3"
+#sur[sur$quartileNum==4,]$quartile <- "Q4"
+#sur$quartile <- factor(sur$quartile, levels=c("Q1", "Q2", "Q3", "Q4"))
+
+# km OS
+smpl.surv <- Surv(sur$os_days, sur$os_status)~sur$quartile
+sigTest <- survdiff(smpl.surv)
+pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+smpl.fit <- survfit(smpl.surv)
+summary(smpl.fit)
+pvalue
+
+#---
+# fancy plot
+# os
+width <- 2500
+#ratio <- 461/572
+ratio <- 547/600
+height <- trunc(width*ratio)
+png(paste0(dirSave, "os.abs.T.cells.CD8.quartiles.2.png"), 
+    width=width, height=height, res=300)
+
+palette <- c("#B79F00", "#00BA38", "#619CFF", "#A58AFF")
+par(mar=c(5.1,5.1,4.1,2.1))
+plot(smpl.fit, lty=c(1,1),
+     xlab="Days", ylab="Overall Survival", col=palette, lwd=2, mark=108, cex=1.5, cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+legend(500,0.5, legend=c(paste0("Q4, n=", q1Num), paste0("Q3, n=", q2Num), paste0("Q2, n=", q3Num), paste0("Q1, n=", q4Num)), 
+       lty=c(1,1), col=rev(palette), lwd=2, cex=1.5)
+
+dev.off()
+# fancy plot
+#---
+
+#n <- 7
+#hcl(h=seq(15, 375-360/n, length=n)%%360, c=100, l=65)
+
+# cox os
+cox <- coxph(Surv(os_days, os_status)~quartile, data=sur)
+summary(cox)
+
+# km DFS
+smpl.surv <- Surv(sur$dfs_days, sur$dfs_status)~sur$quartile
+sigTest <- survdiff(smpl.surv)
+pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+survfit(smpl.surv)
+smpl.fit <- survfit(smpl.surv)
+summary(smpl.fit)
+pvalue
+
+# cox dfs
+cox <- coxph(Surv(dfs_days, dfs_status)~quartile, data=sur)
+summary(cox)
+
 
 #----------------------------------
 # CD8 high and low
@@ -366,6 +469,95 @@ cox <- coxph(Surv(dfs_days, dfs_status)~sum, data=sur)
 summary(cox)
 
 cox <- coxph(Surv(dfs_days, dfs_status)~sum+pathologic_stage+age_at_initial_pathologic_diagnosis+menopause_status+race, data=sur)
+summary(cox)
+
+#----------------------------------
+# CD4 quartiles
+#----------------------------------
+rel <- read.delim("F:\\TNBC TILS\\tnbctils\\brcatcga\\analysis\\R\\cibersort\\data\\CIBERSORT.Output_Abs_fpkm.txt", header=T, stringsAsFactors=F)
+rel$Input.Sample <- gsub("\\.", "-", rel$Input.Sample)
+
+# 5 = CD8, 8 = CD4
+cell_idx <- 8
+cibersort <- rel[,c(1,cell_idx)]
+colnames(cibersort)[2] <- "sum"
+
+sur <- merge(sur, cibersort, by.x="bcr_patient_barcode", by.y="Input.Sample", all.x=T)
+sur <- sur[!(is.na(sur$sum)),]
+
+sur[sur$race=="ASIAN" & !is.na(sur$race),]$race <- NA
+sur$race <- factor(sur$race, levels=c(unique(sur$race)))
+count(sur, 'race')
+
+sur[sur$pathologic_stage %in% c("Stage I", "Stage IA"),]$pathologic_stage <- "Stage I-II"
+sur[sur$pathologic_stage %in% c("Stage II", "Stage IIA", "Stage IIB"),]$pathologic_stage <- "Stage I-II"
+sur[sur$pathologic_stage %in% c("Stage III", "Stage IIIA", "Stage IIIB", "Stage IIIC"),]$pathologic_stage <- "Stage III-IV"
+sur[sur$pathologic_stage %in% c("Stage IV"),]$pathologic_stage <- "Stage III-IV"
+sur$pathologic_stage <- factor(sur$pathologic_stage, levels=c("Stage I-II", "Stage III-IV"), ordered=T)
+count(sur, 'pathologic_stage')
+
+sur[sur$menopause_status %in% c("Indeterminate (neither Pre or Postmenopausal)"),]$menopause_status <- NA
+sur[sur$menopause_status %in% c("Peri (6-12 months since last menstrual period)"),]$menopause_status <- NA
+sur$menopause_status <- factor(sur$menopause_status, levels=c("Pre (<6 months since LMP AND no prior bilateral ovariectomy AND not on estrogen replacement)",
+                                                              "Post (prior bilateral ovariectomy OR >12 mo since LMP with no prior hysterectomy)"), ordered=T)
+count(sur, 'menopause_status')
+
+quantile(sur$sum)
+
+sur$quartile <- cut(sur$sum, breaks=quantile(sur$sum), labels=c("Q1","Q2","Q3","Q4"), include.lowest=T)
+
+q1Num <- dim(sur[sur$quartile=="Q1",])[1]
+q2Num <- dim(sur[sur$quartile=="Q2",])[1]
+q3Num <- dim(sur[sur$quartile=="Q3",])[1]
+q4Num <- dim(sur[sur$quartile=="Q4",])[1]
+
+# km OS
+smpl.surv <- Surv(sur$os_days, sur$os_status)~sur$quartile
+sigTest <- survdiff(smpl.surv)
+pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+smpl.fit <- survfit(smpl.surv)
+summary(smpl.fit)
+pvalue
+
+#n <- 7
+#hcl(h=seq(15, 375-360/n, length=n)%%360, c=100, l=65)
+
+# cox os
+cox <- coxph(Surv(os_days, os_status)~quartile, data=sur)
+summary(cox)
+
+# km DFS
+smpl.surv <- Surv(sur$dfs_days, sur$dfs_status)~sur$quartile
+sigTest <- survdiff(smpl.surv)
+pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+survfit(smpl.surv)
+smpl.fit <- survfit(smpl.surv)
+summary(smpl.fit)
+pvalue
+
+#---
+# fancy plot
+# os
+width <- 2500
+#ratio <- 461/572
+ratio <- 547/600
+height <- trunc(width*ratio)
+png(paste0(dirSave, "dfs.abs.T.cells.CD4.memory.activated.quartiles.2.png"), 
+    width=width, height=height, res=300)
+
+palette <- c("#B79F00", "#00BA38", "#619CFF", "#A58AFF")
+par(mar=c(5.1,5.1,4.1,2.1))
+plot(smpl.fit, lty=c(1,1),
+     xlab="Days", ylab="Disease Free Survival", col=palette, lwd=2, mark=108, cex=1.5, cex.lab=1.5, cex.axis=1.5, cex.main=1.5)
+legend(200,0.5, legend=c(paste0("Q4, n=", q1Num), paste0("Q3, n=", q2Num), paste0("Q2, n=", q3Num), paste0("Q1, n=", q4Num)), 
+       lty=c(1,1), col=rev(palette), lwd=2, cex=1.5)
+
+dev.off()
+# fancy plot
+#---
+
+# cox dfs
+cox <- coxph(Surv(dfs_days, dfs_status)~quartile, data=sur)
 summary(cox)
 
 #----------------------------------
@@ -697,6 +889,74 @@ cox <- coxph(Surv(dfs_days, dfs_status)~sum, data=sur)
 summary(cox)
 
 #----------------------------------
+# M1 macrophages quartiles
+#----------------------------------
+rel <- read.delim("F:\\TNBC TILS\\tnbctils\\brcatcga\\analysis\\R\\cibersort\\data\\CIBERSORT.Output_Abs_fpkm.txt", header=T, stringsAsFactors=F)
+rel$Input.Sample <- gsub("\\.", "-", rel$Input.Sample)
+
+# 5 = CD8, 8 = CD4
+cell_idx <- 16
+cibersort <- rel[,c(1,cell_idx)]
+colnames(cibersort)[2] <- "sum"
+
+sur <- merge(sur, cibersort, by.x="bcr_patient_barcode", by.y="Input.Sample", all.x=T)
+sur <- sur[!(is.na(sur$sum)),]
+
+sur[sur$race=="ASIAN" & !is.na(sur$race),]$race <- NA
+sur$race <- factor(sur$race, levels=c(unique(sur$race)))
+count(sur, 'race')
+
+sur[sur$pathologic_stage %in% c("Stage I", "Stage IA"),]$pathologic_stage <- "Stage I-II"
+sur[sur$pathologic_stage %in% c("Stage II", "Stage IIA", "Stage IIB"),]$pathologic_stage <- "Stage I-II"
+sur[sur$pathologic_stage %in% c("Stage III", "Stage IIIA", "Stage IIIB", "Stage IIIC"),]$pathologic_stage <- "Stage III-IV"
+sur[sur$pathologic_stage %in% c("Stage IV"),]$pathologic_stage <- "Stage III-IV"
+sur$pathologic_stage <- factor(sur$pathologic_stage, levels=c("Stage I-II", "Stage III-IV"), ordered=T)
+count(sur, 'pathologic_stage')
+
+sur[sur$menopause_status %in% c("Indeterminate (neither Pre or Postmenopausal)"),]$menopause_status <- NA
+sur[sur$menopause_status %in% c("Peri (6-12 months since last menstrual period)"),]$menopause_status <- NA
+sur$menopause_status <- factor(sur$menopause_status, levels=c("Pre (<6 months since LMP AND no prior bilateral ovariectomy AND not on estrogen replacement)",
+                                                              "Post (prior bilateral ovariectomy OR >12 mo since LMP with no prior hysterectomy)"), ordered=T)
+count(sur, 'menopause_status')
+
+quantile(sur$sum)
+
+sur$quartile <- cut(sur$sum, breaks=quantile(sur$sum), labels=c("Q1","Q2","Q3","Q4"), include.lowest=T)
+
+q1Num <- dim(sur[sur$quartile=="Q1",])[1]
+q2Num <- dim(sur[sur$quartile=="Q2",])[1]
+q3Num <- dim(sur[sur$quartile=="Q3",])[1]
+q4Num <- dim(sur[sur$quartile=="Q4",])[1]
+
+# km OS
+smpl.surv <- Surv(sur$os_days, sur$os_status)~sur$quartile
+sigTest <- survdiff(smpl.surv)
+pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+smpl.fit <- survfit(smpl.surv)
+summary(smpl.fit)
+pvalue
+
+#n <- 7
+#hcl(h=seq(15, 375-360/n, length=n)%%360, c=100, l=65)
+
+# cox os
+cox <- coxph(Surv(os_days, os_status)~quartile, data=sur)
+summary(cox)
+
+# km DFS
+smpl.surv <- Surv(sur$dfs_days, sur$dfs_status)~sur$quartile
+sigTest <- survdiff(smpl.surv)
+pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+survfit(smpl.surv)
+smpl.fit <- survfit(smpl.surv)
+summary(smpl.fit)
+pvalue
+
+# cox dfs
+cox <- coxph(Surv(dfs_days, dfs_status)~quartile, data=sur)
+summary(cox)
+
+#----------------------------------
 # all immune types continuous for purposes of calculating fdr
 #----------------------------------
 rel <- read.delim("F:\\TNBC TILS\\tnbctils\\brcatcga\\analysis\\R\\cibersort\\data\\CIBERSORT.Output_Abs_fpkm.txt", header=T, stringsAsFactors=F)
@@ -731,6 +991,74 @@ for (cell_idx in 2:(dim(rel)[2]-3)){
   comp2 <- c(cellType, "dfs", "continuous", pvalue)
   pvalTbl[nrow(pvalTbl)+1,] <- comp2
    
+}
+
+cellTblOS <- pvalTbl[pvalTbl$survival=="os",]
+cellTblOS$fdr <- p.adjust(cellTblOS$pval, method="fdr")
+
+cellTblDFS <- pvalTbl[pvalTbl$survival=="dfs",]
+cellTblDFS$fdr <- p.adjust(cellTblDFS$pval, method="fdr")
+
+#----------------------------------
+# all immune types quartiles for purposes of calculating fdr
+#----------------------------------
+rel <- read.delim("F:\\TNBC TILS\\tnbctils\\brcatcga\\analysis\\R\\cibersort\\data\\CIBERSORT.Output_Abs_fpkm.txt", header=T, stringsAsFactors=F)
+rel$Input.Sample <- gsub("\\.", "-", rel$Input.Sample)
+
+pvalTbl <- data.frame(cellType=character(), survival=character(), splitType=character(), pval=numeric())
+
+#cell_idx=2
+for (cell_idx in 2:(dim(rel)[2]-3)){
+  
+  cibersort <- rel[,c(1,cell_idx)]
+  cellType <- colnames(cibersort)[2]
+  colnames(cibersort)[2] <- "sum"
+  
+  surv <- merge(sur, cibersort, by.x="bcr_patient_barcode", by.y="Input.Sample", all.x=T)
+  
+  # analysis of cibersort
+  surv <- surv[!(is.na(surv$sum)),]
+  
+  #surv$rank <- rank(surv$sum, ties.method="first")
+  #surv$quartile <- cut(surv$rank, breaks=quantile(surv$rank), labels=c("Q1","Q2","Q3","Q4"), include.lowest=T)
+  #tmp <- surv[order(surv$sum),c(1,75,76,77)]
+    
+  p <- tryCatch({ # some immune cells have too many 0's such that they can't be split into quartiles
+    
+    surv$quartile <- cut(surv$sum, breaks=quantile(surv$sum), labels=c("Q1","Q2","Q3","Q4"), include.lowest=T)
+    
+    # km OS
+    smpl.surv <- Surv(surv$os_days, surv$os_status)~surv$quartile
+    sigTest <- survdiff(smpl.surv)
+    pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+    
+    }, error = function(e) {
+  
+    return(NA)
+      
+    })
+  
+  comp1 <- c(cellType, "os", "quartile", p)
+  pvalTbl[nrow(pvalTbl)+1,] <- comp1
+
+  p <- tryCatch({
+    
+    surv$quartile <- cut(surv$sum, breaks=quantile(surv$sum), labels=c("Q1","Q2","Q3","Q4"), include.lowest=T)
+
+    # km DFS
+    smpl.surv <- Surv(surv$dfs_days, surv$dfs_status)~surv$quartile
+    sigTest <- survdiff(smpl.surv)
+    pvalue <- 1 - pchisq(sigTest$chisq, length(sigTest$n) - 1)
+        
+  }, error = function(e) {
+    
+    return(NA)
+    
+  })
+    
+  comp2 <- c(cellType, "dfs", "quartile", p)
+  pvalTbl[nrow(pvalTbl)+1,] <- comp2
+  
 }
 
 cellTblOS <- pvalTbl[pvalTbl$survival=="os",]
